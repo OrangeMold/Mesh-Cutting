0555d2f741ef4bad7024854a2e21c5f2347a16df
Calamisia <161609163+Calamisia@users.noreply.github.com>
2024-10-14 12:06:26 -0400

    Updating DrawCut.cs to take a list of points
    I dont have a testing environment so you're gonna have to try it out yourself, it may very well not at all work

--- Assets/Scripts/DrawCut.cs
+++ Assets/Scripts/DrawCut.cs
@@ -1,23 +1,20 @@
-﻿using UnityEngine;
-using Quaternion = UnityEngine.Quaternion;
-using Vector3 = UnityEngine.Vector3;
+using UnityEngine;
+using System.Collections.Generic;
 
-public class DrawCut : MonoBehaviour
+public class DrawCircularCut : MonoBehaviour
 {
-   // public Transform boxVis;
-    Vector3 pointA;
-    Vector3 pointB;
-
+    private List<Vector3> cutPoints; // Stores points along the circular path
     private LineRenderer cutRender;
-    private bool animateCut;
+    private Camera cam;
+    private bool isCutting;
 
-    Camera cam;
-
-    void Start() {
+    void Start()
+    {
         cam = FindObjectOfType<Camera>();
         cutRender = GetComponent<LineRenderer>();
-        cutRender.startWidth = .05f;
-        cutRender.endWidth = .05f;
+        cutRender.startWidth = 0.05f;
+        cutRender.endWidth = 0.05f;
+        cutPoints = new List<Vector3>();
     }
 
     void Update()
@@ -27,56 +24,64 @@ public class DrawCut : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            pointA = cam.ScreenToWorldPoint(mouse);
+            isCutting = true;
+            cutPoints.Clear(); // Reset the list of points
+            AddPointToCut(cam.ScreenToWorldPoint(mouse)); // Add the starting point
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && isCutting)
         {
-            animateCut = false;
-            cutRender.SetPosition(0,pointA);
-            cutRender.SetPosition(1,cam.ScreenToWorldPoint(mouse));
-            cutRender.startColor = Color.gray;
-            cutRender.endColor = Color.gray;
+            AddPointToCut(cam.ScreenToWorldPoint(mouse)); // Add points as the mouse moves
+            UpdateCutRenderer(); // Update visual for the cut line
         }
 
-        if (Input.GetMouseButtonUp(0)) {
-            pointB = cam.ScreenToWorldPoint(mouse);
-            CreateSlicePlane();
-            cutRender.positionCount = 2;
-            cutRender.SetPosition(0,pointA);
-            cutRender.SetPosition(1,pointB);
-            animateCut = true;
-        }
-
-        if (animateCut)
+        if (Input.GetMouseButtonUp(0) && isCutting)
         {
-            cutRender.SetPosition(0,Vector3.Lerp(pointA,pointB,1f));
+            isCutting = false;
+            PerformCircularCut(); // Perform the cut after letting go
         }
     }
 
-    void CreateSlicePlane() 
+    private void AddPointToCut(Vector3 point)
     {
-        Vector3 pointInPlane = (pointA + pointB) / 2;
-        
-        Vector3 cutPlaneNormal = Vector3.Cross((pointA-pointB),(pointA-cam.transform.position)).normalized;
-        Quaternion orientation = Quaternion.FromToRotation(Vector3.up, cutPlaneNormal);
-        //boxVis.rotation = orientation;
-       // boxVis.localScale = new Vector3(10, 0.25f, 10);
-       // boxVis.position = pointInPlane;
-
-        
-        var all = Physics.OverlapBox(pointInPlane, new Vector3(100, 0.01f, 100), orientation);
-        
-        //Ray ray = new Ray(pointA, (pointB - pointA).normalized);
-        //var all = Physics.RaycastAll(ray);
+        if (cutPoints.Count == 0 || Vector3.Distance(cutPoints[cutPoints.Count - 1], point) > 0.1f)
         {
+            cutPoints.Add(point);
+        }
+    }
+
+    private void UpdateCutRenderer()
+    {
+        cutRender.positionCount = cutPoints.Count;
+        cutRender.SetPositions(cutPoints.ToArray());
+        cutRender.startColor = Color.red;
+        cutRender.endColor = Color.red;
+    }
+
+    private void PerformCircularCut()
+    {
+        if (cutPoints.Count < 2) return; // Not enough points to make a cut
+
+        // Loop through pairs of consecutive points and cut between them
+        for (int i = 0; i < cutPoints.Count - 1; i++)
+        {
+            Vector3 pointA = cutPoints[i];
+            Vector3 pointB = cutPoints[i + 1];
+
+            Vector3 pointInPlane = (pointA + pointB) / 2;
+            Vector3 cutPlaneNormal = Vector3.Cross((pointA - pointB), (pointA - cam.transform.position)).normalized;
+            Quaternion orientation = Quaternion.FromToRotation(Vector3.up, cutPlaneNormal);
+
+            var all = Physics.OverlapBox(pointInPlane, new Vector3(100, 0.01f, 100), orientation);
+
             foreach (var hit in all)
             {
                 MeshFilter filter = hit.gameObject.GetComponentInChildren<MeshFilter>();
-                if(filter != null)
+                if (filter != null)
+                {
                     Cutter.Cut(hit.gameObject, pointInPlane, cutPlaneNormal);
+                }
             }
         }
-        
     }
 }
